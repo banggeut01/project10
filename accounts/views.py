64f8dfc46248b4015d20249c124285d945aa9efa
banggeut01 <genie121110@gmail.com>
2019-11-21 17:07:21 +0900

    Add Accounts list page & Detail page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import User
+from django.db.models import Q # 리드미
+def index(request):
+    # users = User.objects.filter(~Q(username='admin'))
+    accounts = User.objects.all()
+    context = {
+        'accounts': accounts
+    }
+    return render(request, 'accounts/index.html', context)
 
-# Create your views here.
+def detail(request, account_pk):
+    account = get_object_or_404(User, id=account_pk)
+    context = {
+        'account': account
+    }
+    return render(request, 'accounts/profile.html', context)
+
+def signup(request):
+    return render(request, 'accounts/form.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect('movies:index')
