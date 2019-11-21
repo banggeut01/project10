51a301ca1e4ecf3558c87fdb82efb5e158058d52
mooyeon-choi <dus1208@ajou.ac.kr>
2019-11-21 17:06:59 +0900

    Add movies/

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'movies'
 urlpatterns = [
-    path('', views.index, name='index')   
+    path('', views.index, name='index'),
+    path('<int:movie_pk>/',views.detail,name='detail'),
+    path('<int:movie_pk>/reviews/new/',views.new,name='new'),
+    path('<int:movie_pk>/reviews/<int:review_pk>/delete/',views.delete,name='delete'),
+    path('<int:movie_pk>/like/',views.like,name='like'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,55 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from .forms import ReviewForm
+from .models import Movie,Review
 # Create your views here.
 def index(request):
-    return render(request, './movies/index.html')
+    movies = Movie.objects.all()
+    context = {
+        'movies' : movies
+    }
+    return render(request,'movies/index.html',context)
+
+def detail(request,movie_pk):
+    movie = get_object_or_404(Movie,pk=movie_pk)
+    review_form = ReviewForm()
+    context = {
+        'movie': movie,
+        'review_form': review_form
+    }
+    return render(request,'movies/detail.html',context)
+
+@require_POST
+def new(request,movie_pk):
+    if request.user.is_authenticated:
+        movie = get_object_or_404(Movie,pk=movie_pk)
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            form = review_form.save(commit=False)
+            form.user = request.user
+            form.movie = movie
+            form.save()
+            return redirect('movies:detail',movie_pk)
+    else:
+        return redirect('accounts:login')
+
+@require_POST
+def delete(request,movie_pk,review_pk):
+    review = get_object_or_404(Review,pk=review_pk)
+    if review.user == request.user:
+        review.delete()
+    return redirect('movies:detail',movie_pk)
+
+@require_POST
+def like(request,movie_pk):
+    movie = get_object_or_404(Movie,pk=movie_pk)
+    if request.user.is_authenticated:
+        if request.user in movie.like_user.all():
+            movie.like_user.remove(request.user)
+        else:
+            movie.like_user.add(request.user)
+            
+        return redirect('movies:detail', movie_pk)
+    else:
+        return redirect('accounts:login')
