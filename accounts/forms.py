10f5cec547d4739de72dd77b71d568054a22163a
banggeut01 <genie121110@gmail.com>
2019-11-21 17:42:13 +0900

    Add Singup, Login, Detail and Follow

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,8 @@
 from django import forms
-from .models import Review
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ('username', 'password', )
-
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = Review
-        exclude = ('user','movie')
+        fields = ('username',)
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import User
-from django.db.models import Q # 리드미
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
+
 def index(request):
-    # users = User.objects.filter(~Q(username='admin'))
     accounts = User.objects.all()
     context = {
         'accounts': accounts
@@ -17,10 +21,41 @@ def detail(request, account_pk):
     return render(request, 'accounts/profile.html', context)
 
 def signup(request):
-    return render(request, 'accounts/form.html')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('movies:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('movies:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
 
 def logout(request):
+    auth_logout(request)
     return redirect('movies:index')
+
+def follow(request, account_pk):
+    user_profile = get_object_or_404(get_user_model(), pk=account_pk)
+    if request.user in user_profile.followers.all(): 
+        user_profile.followers.remove(request.user)
+    else:
+        user_profile.followers.add(request.user)
+    return redirect('accounts:detail', account_pk)
